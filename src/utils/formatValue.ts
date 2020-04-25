ca5d94dbc788597ad3ab411e1cbc3b50159484dd
Ruan Xavier <ruandsxavier@gmail.com>
2020-04-25 13:46:19 -0300

    finishing the dashboard and adding links to navigate

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,23 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (
+  value: number,
+  type: 'income' | 'outcome' | null = null,
+): string => {
+  if (!value) {
+    return '------';
+  }
+  const valueString = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+  let formatedValue = '';
+
+  formatedValue = valueString;
+
+  if (type === 'outcome') {
+    formatedValue = `- ${valueString}`;
+  }
+
+  return formatedValue;
+};
 
 export default formatValue;
